7122800c2e46f24f1736e808797add8922e522e1
Kamal <kamala@gmail.com>
2026-02-06 20:45:57 +0700

    update konten dan tambah halaman about

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function showMessage() {
-  alert("Terima kasih telah mengunjungi portofolio saya!");
+    alert("Terima kasih telah mengunjungi portofolio saya!");
+}
+
+let tombol = document.getElementById("btn");
+if (tombol) {
+    tombol.addEventListener("click", showMessage);
 }
